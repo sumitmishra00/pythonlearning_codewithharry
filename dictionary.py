99ef09df367d7da1b3c65d8125829c2c3dd8725d
Sumit Mishra <sumitmishrayt007@gmail.com>
2025-06-05 01:52:55 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+marks = {
+
+    "Sumit": 99,
+    "Nikhil": 22,
+    "Aku": 44
+}
+
+print(marks, type(marks))
+print(marks["Sumit"])
---
+++ dictionary_method.py
@@ -0,0 +1,15 @@
+a = {
+    "name": "Sumit",
+    "from": "Haryana",
+    "Collge": "SRM",
+    "CGPA": 7.9
+}
+
+print(a)
+print(a.items())
+print(a.keys())
+a.update({"CGPA":8.1})
+print(a)
+print(a.get("from"))
+
+
